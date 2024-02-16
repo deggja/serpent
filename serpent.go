76e9826cf38481a9946febf3a625f4600ed44851
deggja <danieldagfinrud@gmail.com>
2024-02-17 00:32:48 +0100

    feat: edits to readme #minor
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- serpent.go
+++ serpent.go
@@ -225,7 +225,7 @@ func main() {
 	// init k8s client
 	initKubeClient()
 
-	logFile, err := os.OpenFile("debug.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	logFile, err := os.OpenFile("chaos.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
